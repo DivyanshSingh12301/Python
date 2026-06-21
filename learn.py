d14aadbb3eac40f35154c5f02b8b84c9ab1bddea
DivyanshSingh12301 <divyanshsingh12301@gmail.com>
2026-06-21 19:13:13 +0530

    Organized repo into data, notebooks, and scripts folders

--- learn.py
+++
@@ -1,4 +0,0 @@
-#If we want to print double quotes then we can use single quotes instead of double on the outer side and then we can print the double quotes as usual
-#print('hello, "friend"')
-# Or else we can use the back slash
-print("hello, \"friend\"")
